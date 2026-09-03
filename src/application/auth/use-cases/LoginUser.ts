0cc87c090778fdcc342744ac1f54de41dcca58c7
Adithyan <adithyanadi408@gmail.com>
2026-09-03 10:23:27 +0530

    feat: add MySQL database with Prisma user model

---
+++ src/application/auth/use-cases/LoginUser.ts
@@ -0,0 +1,34 @@
+import { IUserRepository } from "../../../domain/repositories/IUserRepository";
+import { IPasswordService } from "../../../domain/services/IPasswordService";
+import { ITokenService } from "../../../domain/services/ITokenService";
+
+export class LoginUser{
+    constructor(
+        private readonly userRepository: IUserRepository,
+        private readonly passwordService: IPasswordService,
+        private readonly tokenService: ITokenService
+    ){}
+
+    async excute(email: string, password: string){
+        const user = await this.userRepository.findByEmail(email)
+        if(!user){
+            throw new Error("Invalid credentials")
+        }
+        const passwordMatches = await this.passwordService.compare(password, user.passwordHash)
+        if(!passwordMatches){
+            throw new Error("invalid credentials")
+        }
+        const accessToken = this.tokenService.generateAccessToken(user.id);
+        const refreshToken = this.tokenService.generateRefreshToken(user.id);
+
+        return{
+            user:{
+                id: user.id,
+                name: user.name,
+                email: user.email
+            },
+            accessToken,
+            refreshToken
+        }
+    }
+}
--- src/application/auth/use-cases/RegisterUser.ts
+++ src/application/auth/use-cases/RegisterUser.ts
@@ -1,21 +1,23 @@
 import { IUserRepository } from "../../../domain/repositories/IUserRepository";
+import { IPasswordService } from "../../../domain/services/IPasswordService";
 
 export class RegisterUser { 
     constructor(
-        private readonly userRepository: IUserRepository
+        private readonly userRepository: IUserRepository,
+        private readonly passwordService: IPasswordService
     ){}
     
     async execute(
         name: string,
         email: string,
-        passwordHash: string
+        password: string
     ){
         const existingUser = await this.userRepository.findByEmail(email);
 
         if(existingUser){
             throw new Error("User already exists");
         }
-
+        const passwordHash = await this.passwordService.hash(password);
         const user = {
             id:crypto.randomUUID(),
             name, 
